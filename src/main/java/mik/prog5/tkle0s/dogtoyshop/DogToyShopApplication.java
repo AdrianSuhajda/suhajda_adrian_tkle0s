3da83b605c22465c2ca903676161c434485b75cd
AdrianSuhajda <adriansuhajda@gmail.com>
2022-12-12 16:04:31 +0100

    chore: init project

---
+++ src/main/java/mik/prog5/tkle0s/dogtoyshop/DogToyShopApplication.java
@@ -0,0 +1,13 @@
+package mik.prog5.tkle0s.dogtoyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogToyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogToyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mik/prog5/tkle0s/dogtoyshop/DogToyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mik.prog5.tkle0s.dogtoyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogToyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
